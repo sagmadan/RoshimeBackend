fa7fc704ec5a9a5c1f4785e6a065152ed21090b8
sagmadan <sagarmadan5@gmail.com>
2024-09-08 03:59:33 +0530

    Starting server after db connection

--- server.js
+++ server.js
@@ -21,11 +21,13 @@ app.use('/api/memories', verifyToken, memoryRoutes); // Protect memory routes wi
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
+  .then(() => {
+    console.log('MongoDB connected');
+    // Start the Server
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
   .catch((err) => console.error('MongoDB connection error:', err));
 
-// Start the Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
